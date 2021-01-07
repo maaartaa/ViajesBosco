a49f022d5f091d38023881c2a12d08f0d3e4f31b
Marta Gonzalez <maaartaa.gon@protonmail.com>
2021-01-07 18:59:52 +0100

    Cuarto commit

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-$(document).ready(function(){
-    var altura = $('.menuinferior').offset().top;
+var menu = document.getElementById('menu'); //Declaración de a quien tiene que realizar las modificaciones
+var altura = menu.offsetTop;// Calcula o mide la distacia que hay entre el inicio y el final de la pagina
 
-    $(window).on('scroll', function(){
-        if ( $(window).scrollTop() > altura ){
-            $('.menu').addClass('menu-fixed');
-        } else {
-            $('.menu').removeClass('menu-fixed');
+    window.addEventListener('scroll', function(){ //Cuando la entana haga scroll realize:
+        if ( window.pageYOffset > altura ){ //Si la distancia total de la pagina supera la altura o lo que es lo mismo el punto deonde te encuentres de la pagina
+            menu.classList.add('menu-fixed'); // añade al menu la clase menu-fixed
+        } else { //sino
+            menu.classList.remove('menu-fixed'); // sino es asi que se quite la clase
         }
     });
-
-});
